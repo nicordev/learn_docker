86fa6ca32f5c93b81b98bbe6ed410797a272fc2e
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2022-05-20 14:56:55 +0200

    add docker-compose env variables

---
+++ docker-compose/env_file/index.php
@@ -0,0 +1,9 @@
+<pre>
+<?php
+echo $_ENV['FRUITS'] . "\n";
+echo $_ENV['QUANTITY'] . "\n";
+echo $_ENV['COLORS'] . "\n";
+echo $_ENV['COMPOSE_PROJECT_NAME'] . "\n";
+echo $_ENV['BANANA_COLOR'] . "\n";
+?>
+</pre>
